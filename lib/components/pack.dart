cd55aac83ca16df015fcf1a8dbd438d9ef5f717c
Ibragim <ibragim.adulazizli@gmail.com>
2020-09-25 16:40:43 +0400

    ui finalllllllllllllll

--- lib/components/pack.dart
+++ lib/components/pack.dart
@@ -1,4 +1,5 @@
 import 'package:auto_animated/auto_animated.dart';
+import 'package:bacg/components/animated_appear.dart';
 import 'package:bacg/components/custom_button.dart';
 import 'package:bacg/components/pack_logo.dart';
 import 'package:flutter/material.dart';
@@ -11,172 +12,153 @@ class Pack extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimateIfVisible(
-      key: Key('${pack.id}'),
-      duration: Duration(milliseconds: 500),
-      builder: (BuildContext context, Animation<double> animation) {
-        // return Text("ff");
-        return FadeTransition(
-          opacity: Tween<double>(
-            begin: 0,
-            end: 1,
-          ).animate(animation),
-          child: SlideTransition(
-            position: Tween<Offset>(
-              begin: Offset(0, -0.1),
-              end: Offset.zero,
-            ).animate(animation),
-            child: SizedBox(
-              height: 230,
-              child: Column(
-                children: [
-                  Stack(
-                    // fit: ,
-                    fit: StackFit.passthrough,
+    return AnimatedAppear(
+      uniqueKey: ('s${pack.id}'),
+      child: SizedBox(
+        height: 230,
+        child: Column(
+          children: [
+            Stack(
+              // fit: ,
+              fit: StackFit.passthrough,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(
                     children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                        ),
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              width: 178,
-                              height: 160,
-                              child: PackLogo(number: pack.id),
-                            ),
-                            Expanded(
-                              child: Container(
-                                // width: double.infinity,
+                      SizedBox(
+                        width: 178,
+                        height: 160,
+                        child: PackLogo(number: pack.id),
+                      ),
+                      Expanded(
+                        child: Container(
+                          // width: double.infinity,
 
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image: AssetImage(
-                                          'assets/broken_gradient.png'),
-                                      fit: BoxFit.cover),
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage('assets/broken_gradient.png'),
+                                fit: BoxFit.cover),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Call count",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .caption,
+                                        ),
+                                        SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text(
+                                          pack.callCount.toString(),
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .subtitle2,
+                                        ),
+                                      ],
+                                    ),
+                                  ],
                                 ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.start,
-                                        children: [
-                                          Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                "Call count",
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .caption,
-                                              ),
-                                              SizedBox(
-                                                height: 5,
-                                              ),
-                                              Text(
-                                                pack.callCount.toString(),
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .subtitle2,
-                                              ),
-                                            ],
-                                          ),
-                                        ],
-                                      ),
-                                      SizedBox(
-                                        height: 10,
-                                      ),
-                                      Row(
-                                        children: [
-                                          Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                "Duration",
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .caption,
-                                              ),
-                                              SizedBox(
-                                                height: 5,
-                                              ),
-                                              Text(
-                                                "${pack.duration} days",
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .subtitle2,
-                                              ),
-                                            ],
-                                          ),
-                                        ],
-                                      ),
-                                      SizedBox(
-                                        height: 10,
-                                      ),
-                                      Row(
-                                        children: [
-                                          Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                "Price",
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .caption,
-                                              ),
-                                              SizedBox(
-                                                height: 5,
-                                              ),
-                                              Text(
-                                                pack.getPrice('az'),
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .subtitle2,
-                                              ),
-                                            ],
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
+                                SizedBox(
+                                  height: 10,
                                 ),
-                              ),
+                                Row(
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Duration",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .caption,
+                                        ),
+                                        SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text(
+                                          "${pack.duration} days",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .subtitle2,
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Price",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .caption,
+                                        ),
+                                        SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text(
+                                          pack.getPrice('az'),
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .subtitle2,
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
-                      )
+                      ),
                     ],
                   ),
-                  SizedBox(
-                    height: 7,
-                  ),
-                  SizedBox(
-                    height: 40,
-                    child: BacgButton(
-                      type: ButtonType.Primary,
-                      onPressed: () {},
-                      text: "Buy",
-                      iconName: 'cart',
-                      // preferredWidth: 300,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 7,
+            ),
+            SizedBox(
+              height: 40,
+              child: BacgButton(
+                type: ButtonType.Primary,
+                onPressed: () {},
+                text: "Buy",
+                iconName: 'cart',
+                // preferredWidth: 300,
               ),
             ),
-          ),
-        );
-      },
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/views/login_components/signin.dart
+++ lib/views/login_components/signin.dart
@@ -111,7 +111,7 @@ class _SignInState extends State<SignIn> {
                   });
                 } else {
                   appState.login(
-                    req.Login(phone: '994515224452', password: 'qwerty'),
+                    req.Login(phone: '5224452', password: 'qwerty'),
                     // req.Login(phone: _phoneController.text, password: _passController.text)
                   );
                 }
