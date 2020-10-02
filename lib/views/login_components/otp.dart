1f81aa773bffa0d9ccf819e9af9a22b8d1fffb2d
Farhad <farkhad.goja@gmail.com>
2020-10-02 17:49:11 +0400

    kak dushe ugodno

--- lib/views/login_components/otp.dart
+++ lib/views/login_components/otp.dart
@@ -30,102 +30,114 @@ class _OtpState extends State<Otp> {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<OtpStateModel>(
       create: (context) => OtpStateModel(type: OtpType.Registration),
-      child: Consumer<OtpStateModel>(builder: (context, state, widget) {
-        final timeRemaining = state.secondsRemaining;
-        final mins = (timeRemaining / 60).floor();
-        final secs = timeRemaining % 60;
-        final phone =
-            Provider.of<AppState>(context).registrationPhone.fullNumber;
-        // print(mins);
-        return Consumer<ScreenHeight>(builder: (context, heightState, widget) {
-          _keyboardHeight = heightState.keyboardHeight;
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "$phone number verification",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                  fontFamily: "Montserrat",
+      child: Consumer<OtpStateModel>(
+        builder: (context, state, widget) {
+          final timeRemaining = state.secondsRemaining;
+          final mins = (timeRemaining / 60).floor();
+          final secs = timeRemaining % 60;
+          final phone =
+              Provider.of<AppState>(context).registrationPhone.fullNumber;
+          // print(mins);
+          return Consumer<ScreenHeight>(
+              builder: (context, heightState, widget) {
+            _keyboardHeight = heightState.keyboardHeight;
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-
-              // Text(phone),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: _otpController,
-                decoration: InputDecoration(
-                  labelText: "Enter OTP code",
-                  labelStyle: TextStyle(color: Colors.white),
-                  fillColor: Colors.black45,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.transparent),
-                    borderRadius: BorderRadius.circular(5),
+                Text(
+                  "$phone number verification",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: "Montserrat",
                   ),
                 ),
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              FlatButton(
-                onPressed: timeRemaining > 0
-                    ? null
-                    : () {
-                        Provider.of<AppState>(context, listen: false)
-                            .setLoginStage(LoginStage.SignUp);
-                        // state.restartOtpTimer();
-                      },
-                child: Text(
-                  timeRemaining > 0
-                      ? "You can resend code or fix the phone number in $mins:${formatter.format(secs)}" //Localized("you_can_resend_code").value
-                      : "Resend code",
-                  style:
-                      TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
-                  textAlign: TextAlign.left,
+                SizedBox(
+                  height: 10,
                 ),
-              ),
-              // BacgButton(
-              //     onPressed: () {
-              //       state.restart();
-              //     },
-              //     type: ButtonType.Primary,
-              //     text: "Resend"),
-              // Expanded(
-              //   child: Container(),
-              // ),
 
-              SizedBox(
-                height: _getStaticGapSize(),
-              ),
-              BacgButton(
-                text: "SEND",
-                type: ButtonType.Primary,
-                onPressed: () async {
-                  if (await Provider.of<AppState>(context, listen: false)
-                      .verifyOtp(_otpController.text, OtpType.Registration)) {
-                    state.stop();
-                  }
-                },
-              )
-            ],
-          );
-        });
-      }),
+                // Text(phone),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  controller: _otpController,
+                  decoration: InputDecoration(
+                    labelText: "Enter OTP code",
+                    labelStyle: TextStyle(color: Colors.white),
+                    fillColor: Colors.black45,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                FlatButton(
+                  height: 48,
+                  minWidth: double.infinity,
+                  shape: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(3),
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: timeRemaining > 0 ? Colors.grey : Colors.green,
+                    ),
+                  ),
+                  onPressed: timeRemaining > 0
+                      ? null
+                      : () {
+                          Provider.of<AppState>(context, listen: false)
+                              .setLoginStage(LoginStage.SignUp);
+                          // state.restartOtpTimer();
+                        },
+                  child: Text(
+                    timeRemaining > 0
+                        ? "You can resend code or fix the phone number in $mins:${formatter.format(secs)}" //Localized("you_can_resend_code").value
+                        : "Resend code",
+                    style: TextStyle(
+                        color: Colors.white, fontFamily: 'Montserrat'),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                // BacgButton(
+                //     onPressed: () {
+                //       state.restart();
+                //     },
+                //     type: ButtonType.Primary,
+                //     text: "Resend"),
+                // Expanded(
+                //   child: Container(),
+                // ),
+
+                SizedBox(
+                  height: _getStaticGapSize(),
+                ),
+                BacgButton(
+                  text: "SEND",
+                  type: ButtonType.Primary,
+                  onPressed: () async {
+                    if (await Provider.of<AppState>(context, listen: false)
+                        .verifyOtp(_otpController.text, OtpType.Registration)) {
+                      state.stop();
+                    }
+                  },
+                )
+              ],
+            );
+          });
+        },
+      ),
     );
     // final mins = (remainingTime / 60).floor();
     // final secs = remainingTime % 60;
